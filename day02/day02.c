68af3bbdde65ca869a381bf7d679137b3d8d9050
Apple <Apple@Apples-MacBook.local>
2025-09-12 13:38:34 +0530

    Day 02: Added rectangle area and perimeter program

---
+++ day02/day02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+    int length,breadth,area,perimeter;
+    printf("length of rectangle: \n");
+    scanf("%d", &length);
+    printf("breadth of rectangle: \n");
+    scanf("%d", &breadth);
+    area=(length*breadth);
+    perimeter=2*(length+breadth);
+    printf("area and perimeter of a reactangle with dimentions %d and %d is %d and %d",length,breadth,area,perimeter);
+    return 0;
+}
