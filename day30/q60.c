07d5ea6a2bbd4417c9de8568ec898102cf30641c
mayanksoni-alt <mayank.soni3@icloud.com>
2025-11-03 10:00:47 +0530

    Add day30 program

---
+++ day30/q60.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, pos = 0, neg = 0, zero = 0;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(arr[i] > 0)
+            pos++;
+        else if(arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d", pos, neg, zero);
