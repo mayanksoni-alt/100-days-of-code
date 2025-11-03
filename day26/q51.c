e62932a10a04377df27345aab362a45ef642f3bd
mayanksoni-alt <mayank.soni3@icloud.com>
2025-11-03 09:39:36 +0530

    Add day26 program

---
+++ day26/q51.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k, n = 5;
+    for (i = 1; i <= n; i++) {
+        for (j = n - i; j > 0; j--)
+            printf(" ");
+        for (k = n - i + 1; k <= n; k++)
+            printf("%d", k);
+        printf("\n");
+    }
+    return 0;
+}
