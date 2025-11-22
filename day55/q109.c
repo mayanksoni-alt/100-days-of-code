5bf0398a3c97b9b7d122e8ec8c522951b5bd5c67
mayanksoni-alt <mayank.soni3@icloud.com>
2025-11-22 16:22:46 +0530

    Add day55 prograam

---
+++ day55/q109.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+
+    int windowSum = 0;
+
+    for (int i = 0; i < k; i++)
+        windowSum += arr[i];
+
+    int maxSum = windowSum;
+
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i] - arr[i - k];
+        if (windowSum > maxSum)
+            maxSum = windowSum;
+    }
+
+    printf("%d", maxSum);
+
+    return 0;
+}
