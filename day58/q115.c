ccf525f0ff6c5fb81fc25da8ad76bc2403ccde2d
mayanksoni-alt <mayank.soni3@icloud.com>
2025-11-22 16:41:51 +0530

    Add day58 prograam

---
+++ day58/q115.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char s[200], t[200];
+    scanf("%s", s);
+    scanf("%s", t);
+
+    int freq[26] = {0};
+
+    for (int i = 0; s[i] != '\0'; i++)
+        freq[s[i] - 'a']++;
+
+    for (int i = 0; t[i] != '\0'; i++)
+        freq[t[i] - 'a']--;
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            printf("Not Anagram");
+            return 0;
+        }
+    }
+
+    printf("Anagram");
+    return 0;
+}
