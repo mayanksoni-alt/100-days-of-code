3890c446b455ba7fcc8f41098e6c1bc355084bdf
mayanksoni-alt <mayank.soni3@icloud.com>
2025-11-23 19:50:17 +0530

    Add day51 progrm

---
+++ day51/q101.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    scanf("%d", &target);
+
+    int first = -1, last = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) {
+            first = i;
+            break;
+        }
+    }
+    for(int i = n - 1; i >= 0; i--) {
+        if(arr[i] == target) {
+            last = i;
+            break;
+        }
+    }
+
+    printf("%d,%d", first, last);
+    return 0;
+}
